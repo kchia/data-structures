da34f8aab8a9534259c57ec9b5ef08be7837c917
kchia <kchia87@gmail.com>
2014-12-10 15:12:06 -0800

    Implements stack using functional and functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  newStack.counter = 0;
+  _.extend(newStack,stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.counter] = value;
+  this.counter ++;
+}
 
+stackMethods.pop = function(){
+  if(this.counter > 0 ){
+    this.counter--;
+    var value = this.storage[this.counter];
+    delete this.storage[this.counter];
+    return value;
+  }
+}
+
+stackMethods.size = function(){
+  return this.counter;
+}
