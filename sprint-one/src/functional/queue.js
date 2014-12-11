41e706b6dab03f9740945c8bbf65e7f1503e0f79
kchia <kchia87@gmail.com>
2014-12-10 16:40:41 -0800

    Some Work

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0; // count the number of objects storage
+  var first = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    // first in first out
+    // adding value to the end
+    // and increment counter
   };
 
   someInstance.dequeue = function(){
+    // remove value from the front
+    // return that value
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
