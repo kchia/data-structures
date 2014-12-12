75ffaa7e620579af456218381f2b4a23994ff72e
kchia <kchia87@gmail.com>
2014-12-12 11:03:12 -0800

    Implements tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,24 +2,29 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push( Tree(value) );
 };
 
 treeMethods.contains = function(target){
-
+  var found = false;
+  var inspect = function (tree) {
+    if (tree.value === target) {
+      found = true;
+    }
+    for (var i = 0; i < tree.children.length; i++) {
+      inspect(tree.children[i]);
+    }
+  };
+  inspect(this);
+  return found;
 };
 
 
