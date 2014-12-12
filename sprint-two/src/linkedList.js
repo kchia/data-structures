d1decd704429fee9067f24b3d8d2609a19d77c3f
kchia <kchia87@gmail.com>
2014-12-11 16:23:59 -0800

    Implements linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.head === null){
+    //if the list is empty, make the new node both head and tail
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    list.tail.next = newNode;
+    list.tail = newNode;
+    return newNode;
   };
 
   list.removeHead = function(){
+    if(list.head !== null){
+      var value = list.head.value;
+      list.head = list.head.next;
+      if(list.head.next === null){
+        list.tail = null;
+      }
+    }
+    return value;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    var found = false;
+
+    while ( currentNode !== list.tail ) {
+      if (currentNode.value === target) {
+        found = true;
+      }
+      currentNode = currentNode.next;
+    }
+    if (currentNode === list.tail && list.tail !== null) {
+      if (currentNode.value === target) {
+        found = true;
+      }
+    }
+
+    return found;
   };
 
   return list;
