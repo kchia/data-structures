60886db674aaf16dbbbe48adc22e088bffbdbf47
kchia <kchia87@gmail.com>
2014-12-13 17:43:38 -0800

    Implements hashTable resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,133 +1,121 @@
 var HashTable = function(){
   this._limit = 8;
-  this._counter = 0;
+  this._count = 0;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(i);
-  var tuple = [k,v];
-
-  if (this._counter >= this._limit * 0.75 ){
-    this._limit = this._limit * 2;
-  }
-
-//Do two checks
-  // First, check whether the bucket exists
-
-      // If it doesn't, then create a bucket with the tuple in it without
-          //interacting with the limitedArray
-  if (bucket === undefined || bucket === null){
+  // hash k
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple;
+  // check if bucket exists
+  var bucket = this._storage.get(index);
+  // if bucket doesnt exist,
+  if (!bucket) {
+    // create a bucket
     bucket = [];
+    // push tuple into bucket
+    tuple = [k, v];
     bucket.push(tuple);
+    // increment count
+    this._storage.set(index,bucket);
+    this._count++;
+    // check whether resize is needed
+    if (this._count > 0.75 * this._limit) {
+      this.resize();
+    };
+    return;
   }
-
-  if (bucket){
-    var found = false;
-    for(var j = 0; j < bucket.length; j++){
-      // If it does, then check if key is in one of the tuples in the bucket
-          // If so, update the tuple's value
-      if(bucket[j][0] === k){
-        bucket[j][1] = v;
-        found = true;
+  // if bucket exists
+  if (bucket) {
+    // iterate over tuples to check if k matches key in tuple
+    for (var i = 0; i < bucket.length; i++) {
+      // if it matches, update tuple's value
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+        this._storage.set(index,bucket);
+        break;
       }
-    }
-    // If not, push the tuple
-    if(!found){
+      // if no match, push tuple into the bucket
+      tuple = [k, v];
       bucket.push(tuple);
+      // increment count
+      this._storage.set(index,bucket);
+      this._count++;
+    // check whether resize is needed
+      if (this._count > 0.75 * this._limit) {
+        this.resize(this._limit * 2);
+      };
     }
-
   }
-
-  // Last, store the bucket we just created or modified in step 1 in limitedArray at index i
-  this._storage.set(i,bucket);
-  this._counter++;
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  // Pull bucket from index i
-  var bucket = this._storage.get(i);
-  // if bucket has only one tuple, return value of that tuple
-  // if bucket is undefined or null or empty
-  if(bucket === undefined || bucket === null || bucket === []){
+  // hash k
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple;
+  // check if bucket exists
+  var bucket = this._storage.get(index);
+  // if bucket doesnt exist,
+  if (!bucket) {
     return null;
   }
-
-  // if bucket is defined, check each tuple
-  if(bucket){
-    for(var j = 0; j < bucket.length; j++){
-    // if tuple is undefined
-      if(bucket[j] === undefined){
-      // return null
-        return null;
-      }
-    // if the tuple's key matches k
-      if(bucket[j][0] === k){
-      // return the tuple's value
-        return bucket[j][1];
-      }
+  // if bucket exists
+  // iterate over tuples to check if k matches key in tuple
+  for (var i = 0; i < bucket.length; i++) {
+    // if it matches, return tuple's value
+    tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
     }
   }
-
-
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(i);
-
-  // if bucket is undefined, empty, or null
-  if(bucket === undefined || bucket === null || bucket === []){
+  // hash k
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple;
+  // check if bucket exists
+  var bucket = this._storage.get(index);
+  // if bucket doesnt exist,
+  if (!bucket) {
     return null;
   }
-  // if there's a non empty bucket
-  if (bucket) {
-    // iterate through each tuple
-    for(var j = 0; j < bucket.length; j++){
-      // if tuple's key matches k, delete the tuple
-      if(bucket[j][0] === k){
-        delete bucket[j];
-      }
+  // if bucket exists
+  // iterate over tuples to check if k matches key in tuple
+  for (var i = 0; i < bucket.length; i++) {
+    // if it matches, return tuple's value
+    tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i,1);
+      this._count--;
+      if (this._count < 0.25 * this._limit) {
+        this.resize(this._limit * 1/2);
+      };
     }
   }
-
-  // Store the modified bucket inside storage
-  this._storage.set(i,bucket);
-
-  this._counter--;
-
-  this.checkSize();
 };
 
-HashTable.prototype.checkSize = function(){
-  //console.log(this._counter, this._limit);
-  if (this._counter < this._limit * 0.25 ){
-    // Before halving the limit, move the buckets with over-the-limit indexes
-    var oldLimit = this._limit;
-    var newLimit = this._limit * 0.5;
-    var tuples = [];
-    for(var l = newLimit; l < oldLimit; l++ ){
-      console.log(l)
-      var bucket = this._storage.get(l);
-      // if bucket has tuples, store the tuples in an array
-      if(bucket){
-        for(var j = 0; j < bucket.length ;j++){
-          tuples.push(bucket[j]);
-        // Remove the tuples from the bucket
-          this.remove(bucket[j][0]);
-        }
+HashTable.prototype.resize = function(newLimit){
+  // Save copy of existing storage
+  var oldStorage = this._storage;
+  var context = this;
+  // Reset the storage with the new limit and counter
+  this._limit = newLimit;
+  this._storage = LimitedArray(newLimit);
+  this._count = 0;
+  // Iterate through copy of storage
+  oldStorage.each(function(bucket){
+    // Insert each tuple into storage
+    if (bucket !== undefined){
+      for(var i = 0; i < bucket.length; i++){
+        context.insert(bucket[i][0], bucket[i][1]);
       }
     }
-    //console.log(tuples)
-    // Halve the limit
-    this._limit = this._limit * 0.5;
-    // Insert the stored tuples
-    for(var m = 0; m < tuples.length; m++){
-      this.insert(tuples[m][0],tuples[m][1]);
-    }
-  }
+  });
+
+
 };
 
 /*
